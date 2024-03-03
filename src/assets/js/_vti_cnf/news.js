68dae0c01e2a7a9236a4d50f7cf2e30906cf1ac8
Leondro Lio <github@leondrolio.com>
2024-03-03 00:27:14 +0000

    Update HTML extension

--- src/assets/js/_vti_cnf/news.js
+++ src/assets/js/_vti_cnf/news.js
@@ -3,4 +3,4 @@ vti_timelastmodified:TR|02 Mar 2024 23:45:13 -0000
 vti_extenderversion:SR|6.0.2.5516
 vti_cacheddtm:TX|02 Mar 2024 23:45:13 -0000
 vti_filesize:IR|0
-vti_backlinkinfo:VX|index.htm assets/templates/pilksoc.dwt 404.htm
+vti_backlinkinfo:VX|assets/templates/pilksoc.dwt 404.html index.html
